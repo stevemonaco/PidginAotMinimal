02441d53e036dfd92d33064eb927b66cc50a0a8b
Stephen Monaco <stevemonaco@hotmail.com>
2023-07-13 14:02:15 -0400

    Simplify

---
+++ PidginAotMinimal/Parsers.cs
@@ -0,0 +1,34 @@
+﻿using Pidgin;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+namespace PidginAotMinimal;
+
+public record Label(string Name, int LineBreaks);
+
+public static class Parsers
+{
+    private static readonly Parser<char, string> _endLine = String(@"\n");
+    private static readonly Parser<char, char> _idChar = Token(IsIdentifierChar);
+    private static readonly Parser<char, string> _idParser = _idChar.AtLeastOnceString();
+
+    public static Parser<char, Label> Parser { get; }
+
+    static Parsers()
+    {
+        // Does not trigger AOT warning
+        //Parser = _idParser
+        //    .Between(Char('['), Char(']'))
+        //    .Select(x => new Label(x, 0));
+
+        // IL3054
+        Parser = _idParser
+            .Between(Char('['), Char(']'))
+            .Then(_endLine.Many(), (t, u) => new Label(t, u.Count()));
+    }
+
+    private static bool IsIdentifierChar(char c)
+    {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    }
+}
--- PidginAotMinimal/Program.cs
+++ PidginAotMinimal/Program.cs
@@ -1,10 +1,6 @@
 ﻿using Pidgin;
 using PidginAotMinimal;
 
-string entryInput = "80=ab";
-var entry = Working.Parser.Parse(entryInput).Value;
-Console.WriteLine($"{entry.Hex}, {entry.Text}");
-
-string labelInput = "[end]";
-var label = Broken.Parser.Parse(labelInput).Value;
-Console.WriteLine($"{label.Name}");
+string labelInput = @"[end]\n";
+var label = Parsers.Parser.Parse(labelInput).Value;
+Console.WriteLine($"{label.Name} {label.LineBreaks}");
