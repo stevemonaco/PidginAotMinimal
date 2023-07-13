c9bf4263b26a5ceb715bd442989be0adb3c185f5
Stephen Monaco <stevemonaco@hotmail.com>
2023-07-13 13:10:54 -0400

    Add project files.

---
+++ PidginAotMinimal/Broken.cs
@@ -0,0 +1,28 @@
+﻿using Pidgin;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+namespace PidginAotMinimal;
+
+public record Label(string Name, int LineBreaks);
+
+public static class Broken
+{
+    private static readonly Parser<char, string> _endLine = String(@"\n").Labelled("End of Line ('\\n')");
+    private static readonly Parser<char, char> _idChar = Token(IsIdentifierChar).Labelled("Identifier character ([0-9][a-z][A-Z])");
+    private static readonly Parser<char, string> _idParser = _idChar.AtLeastOnceString().Labelled("Id");
+
+    public static Parser<char, Label> Parser { get; }
+
+    static Broken()
+    {
+        Parser = _idParser
+            .Between(Char('[').Labelled("Open ('[')"), Char(']').Labelled("Close (']')"))
+            .Then(_endLine.Many(), (t, u) => new Label(t, u.Count()));
+    }
+
+    private static bool IsIdentifierChar(char c)
+    {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    }
+}
---
+++ PidginAotMinimal/Program.cs
@@ -0,0 +1,10 @@
+﻿using Pidgin;
+using PidginAotMinimal;
+
+string entryInput = "80=ab";
+var entry = Working.Parser.Parse(entryInput).Value;
+Console.WriteLine($"{entry.Hex}, {entry.Text}");
+
+string labelInput = "[end]";
+var label = Broken.Parser.Parse(labelInput).Value;
+Console.WriteLine($"{label.Name}");
